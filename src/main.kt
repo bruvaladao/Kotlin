9fcf881d8a5e88c37834b894cf59c68d12ffa021
Bruno Valadao <bru.valadao89@gmail.com>
2021-12-31 16:42:02 -0300

    Projeto heranca

--- src/main.kt
+++ src/main.kt
@@ -1,79 +1,144 @@
 fun main() {
-    println("Bem vindo ao Bytebank.")
+    println("Bem vindo ao Bytebank")
 
-    val contaAlex = Conta()
-    contaAlex.titular = "Alex"
-    contaAlex.numero = 1000
-    contaAlex.setSaldo(100.0)
+    val contaAlex = Conta(titular = "Alex", numero = 1000)
+    contaAlex.deposita(200.0)
 
-    val contaFran = Conta()
-    contaFran.titular = "Fran"
-    contaFran.numero = 1001
-    contaFran.setSaldo(300.0)
+    val contaFran = Conta(numero = 1001, titular = "Fran")
+    contaFran.deposita(300.0)
 
-    println("INFORMAÇÃO CONTA ALEX")
-    println("Titular: ${contaAlex.titular}")
-    println("Número da conta: ${contaAlex.numero}")
-    println("Saldo: ${contaAlex.getSaldo()}")
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
 
-    println("INFORMAÇÃO CONTA FRAN")
-    println("Titular: ${contaFran.titular}")
-    println("Número da conta: ${contaFran.numero}")
-    println("Saldo: ${contaFran.getSaldo()}")
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
 
-    println("Despositando na conta do Alex")
-    contaAlex.deposita(20.0)
-    println(contaAlex.getSaldo())
+    println("depositando na conta do Alex")
+    contaAlex.deposita(50.0)
+    println(contaAlex.saldo)
 
-    println("Depositando na conta da Fran")
-    contaFran.deposita(100.0)
-    println(contaFran.getSaldo())
+    println("depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
 
-    println("Sacando na conta do Alex")
-    contaAlex.saca(20.0)
-    println("Saldo da conta do Alex: ${contaAlex.getSaldo()}")
+    println("sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.saldo)
 
-    println("Transferindo para Fran")
-    if (contaAlex.transfere(50.0, contaFran)){
-        println("Transferencia com sucesso")
-    } else{
+    println("sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("saque em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.saldo)
+
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
+
+    println("Transferência da conta da Fran para o Alex")
+
+    if (contaFran.transfere(destino = contaAlex, valor = 300.0)) {
+        println("Transferência sucedida")
+    } else {
         println("Falha na transferência")
     }
 
-    println("Saldo da conta do Alex: ${contaAlex.getSaldo()}")
-    println("Saldo da conta da Fran: ${contaFran.getSaldo()}")
-
+    println(contaAlex.saldo)
+    println(contaFran.saldo)
 }
 
-class Conta {
-    var titular = ""
-    var numero = 0
-    private var saldo = 0.0
+class Conta(
+    var titular: String,
+    val numero: Int
+) {
+    var saldo = 0.0
+        private set
 
     fun deposita(valor: Double) {
-        this.saldo += valor
+        if (valor > 0) {
+            this.saldo += valor
+        }
     }
 
     fun saca(valor: Double) {
-        if (this.saldo >= valor) {
-            this.saldo -= valor
+        if (saldo >= valor) {
+            saldo -= valor
         }
     }
 
     fun transfere(valor: Double, destino: Conta): Boolean {
-        if (this.saldo >= valor) {
+        if (saldo >= valor) {
             saldo -= valor
             destino.deposita(valor)
             return true
         }
         return false
     }
+}
 
-    fun getSaldo(): Double{
-        return saldo
+fun testaCopiasEReferencias() {
+    val numeroX = 10
+    var numeroY = numeroX
+    numeroY++
+
+    println("numeroX $numeroX")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta("João", 1002)
+    contaJoao.titular = "João"
+    var contaMaria = Conta("Maria", 1003)
+    contaMaria.titular = "Maria"
+    contaJoao.titular = "João"
+
+    println("titular conta joao: ${contaJoao.titular}")
+    println("titular conta maria: ${contaMaria.titular}")
+
+    println(contaJoao)
+    println(contaMaria)
+}
+
+fun testaLacos() {
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
     }
 
-    fun setSaldo(valor: Double){
-        saldo = valor
+    for (i in 5 downTo 1) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+    }
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
     }
 }
