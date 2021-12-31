c92ee2580d786b5fec63c36dd716d87c8ed82c28
Bruno Valadao <bru.valadao89@gmail.com>
2021-12-31 17:40:46 -0300

    Criação da classe Funcionario

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,7 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    testaComportamentosConta()
+    val bruno = Funcionario("Bruno", "370.380.238-37", 1000.0)
+    println("Nome: ${bruno.nome}")
+    println("CPF: ${bruno.cpf}")
+    println("Salário: ${bruno.salario}")
 }
