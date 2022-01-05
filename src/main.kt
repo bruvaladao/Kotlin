c23e6405a2981de3860db74fa3fe9d458dadae73
Bruno Valadao <bru.valadao89@gmail.com>
2022-01-05 20:48:55 -0300

    conta corrente e poupanca

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,23 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaFuncionarios()
+    val contaCorrente = ContaCorrente("Alex", 1000)
+    val contaPoupanca = ContaPoupanca("Fran", 1001)
 
-}
+    contaPoupanca.deposita(1000.0)
+    contaCorrente.deposita(1000.0)
 
+    println("Saldo corrente: ${contaCorrente.saldo}")
+    println("Saldo poupança: ${contaPoupanca.saldo}")
 
+    contaCorrente.saca(100.0)
+    contaPoupanca.saca(100.0)
+
+    println("Saldo corrente pos: ${contaCorrente.saldo}")
+    println("Saldo poupança pos: ${contaPoupanca.saldo}")
+
+    contaCorrente.transfere(100.0, contaPoupanca)
+
+    println("Saldo corrente trans: ${contaCorrente.saldo}")
+    println("Saldo poupança trans: ${contaPoupanca.saldo}")
 }
